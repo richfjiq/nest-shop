73baff8ab9070899cb79271efc4d4072bfa5b741
richfjiq <rfjiq1986@gmail.com>
2024-02-24 21:39:59 -0600

    chore: verifies user role

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -12,7 +19,25 @@ export class UserRoleGuard implements CanActivate {
       'roles',
       context.getHandler(),
     );
-    console.log({ validRoles });
-    return true;
+
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) {
+      throw new BadRequestException('User not found.');
+    }
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} needs a valid role: [${validRoles}]`,
+    );
   }
 }
